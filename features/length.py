af6b74111c9e0e1cd59d63f1bd30f56481a38a83
James Stone <jamstone@arizona.edu>
2026-02-19 19:04:54 -0700

    Adding Utterance Length feature

--- features/length.py
+++ features/length.py
@@ -1,1 +1,7 @@
-"# Utterance length feature" 
+# Utterance length feature
+from tokenizer import *
+
+def UtteranceLength(utterance: str, punctuationIncluded = False) -> int:
+    tokens = tokenizeSent(utterance, PunctuationRemoved=not punctuationIncluded)
+    return len(tokens)
+
